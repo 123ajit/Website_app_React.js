01d9675ef3f816c78dec23de918014d4e3eea947
Ajit Kumar Gupta <ajit45288@gmail.com>
2022-05-07 16:03:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Second from "./components/Second";
+import Section from "./components/Section";
+import Third from "./components/Third";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Section />
+      <Second />
+      <Third />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
